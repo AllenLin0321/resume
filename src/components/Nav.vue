aa217308206eb4e5227858cf632b2b426c0f1141
Allen <glux7381@gmail.com>
2019-06-25 15:21:44 +0800

    Finish Nav Bar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,8 @@
 <template>
-  <v-container class="navbar">
-    <v-layout align-center column justify-end >
-      <v-flex class="xs12">
+  <v-container class="site-header pa-0">
+    <!-- First Session -->
+    <v-layout align-center column>
+      <v-flex class="xs12 header-image mt-5">
         <v-avatar :size="avatar.img_size">
           <img
             src="https://lmpixels.com/wp/kerge-wp/demo2dark/wp-content/uploads/sites/8/2018/03/my_photo.png"
@@ -9,9 +10,31 @@
           >
         </v-avatar>
       </v-flex>
-      <v-flex xs12 mt-4>
-        <h2>林逸倫 Allen</h2>
-        <h3>Front-End Developer</h3>
+      <v-flex xs12 mt-4 class="text-xs-center site-title-block">
+        <h2 class="site-title headline">林逸倫 Allen</h2>
+        <h3 class="site-subtitle subheading my-2">Front-End Developer</h3>
+      </v-flex>
+    </v-layout>
+
+    <!-- Second Session -->
+    <v-layout align-center column mt-4>
+      <v-flex class="site-nav xs12">
+        <v-layout py-2 class="site-nav--item" v-for="(item, index) in nav_items" :key="index">
+          <v-spacer></v-spacer>
+          <v-flex>
+            <v-icon dark :color="item.color">{{item.icon}}</v-icon>
+            <span class="subheading ml-2">{{item.content}}</span>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+
+    <!-- Third Session -->
+    <v-layout align-center column mt-4>
+      <v-flex class="site-contact xs12">
+        <v-btn fab small color="#444" v-for="(contact, index) in site_contact" :key="index">
+          <v-icon color="grey">{{ contact.icon }}</v-icon>
+        </v-btn>
       </v-flex>
     </v-layout>
   </v-container>
@@ -24,7 +47,52 @@ export default {
       avatar: {
         img_size: 110
       },
-      
+      nav_items: [
+        {
+          icon: "home",
+          color: "#F44336",
+          content: "Home"
+        },
+        {
+          icon: "person",
+          color: "#008073",
+          content: "About"
+        },
+        {
+          icon: "trending_up",
+          color: "#05BDD5",
+          content: "Experience"
+        },
+        {
+          icon: "web",
+          color: "#3F51B5",
+          content: "Project"
+        },
+        {
+          icon: "equalizer",
+          color: "#9C27B0",
+          content: "Skills"
+        },
+        {
+          icon: "description",
+          color: "#795548",
+          content: "Resume"
+        }
+      ],
+      site_contact: [
+        {
+          icon: "fas fa-envelope",
+          link: null
+        },
+        {
+          icon: "fab fa-linkedin-in",
+          link: null
+        },
+        {
+          icon: "fab fa-github",
+          link: null
+        }
+      ]
     };
   }
 };
@@ -32,9 +100,21 @@ export default {
 
 
 <style lang="scss" scoped>
-.navbar {
+.site-header {
   background-color: #222222;
   height: 100vh;
   color: white;
+}
+
+.site-nav {
+  width: 100%;
+
+  &--item {
+    cursor: pointer;
+    &:hover {
+      background-color: #333333;
+      border-right: 2px solid #ffcd38;
+    }
+  }
 }
 </style>
